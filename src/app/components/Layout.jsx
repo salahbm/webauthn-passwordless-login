68188b9d39196d26d389750c6a4172951ac74e78
Mukhammad Bakhriddinov <salahbm.001@gmail.com>
2023-07-11 12:21:36 +0900

    layout fixed

--- src/app/components/Layout.jsx
+++ src/app/components/Layout.jsx
@@ -1,11 +1,29 @@
+"use client";
 import React from "react";
 import Animation from "./Animation/Animation";
 
 function Layout({ children }) {
   return (
     <>
-      <Animation />
-      {children}
+      <div className="background-animation">
+        <Animation />
+      </div>
+      <div className="content">{children}</div>
+      <style jsx>{`
+        .background-animation {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          z-index: -1;
+        }
+
+        .content {
+          position: relative;
+          z-index: 1;
+        }
+      `}</style>
     </>
   );
 }
