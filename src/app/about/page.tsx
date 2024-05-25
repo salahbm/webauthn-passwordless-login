d59433d89075e61aebbc005e4720f716dccbe997
salahbm <salahbm.001@gmail.com>
2024-05-25 23:07:31 +0900

    [fixed]: build error

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React from 'react';
 
 const About = () => {
-  const githubRepoUrl = "https://github.com/salahbm/webauthn-passwordlessLogin";
+  const githubRepoUrl =
+    'https://github.com/salahbm/webauthn-passwordless-login';
   return (
     <div
       style={{
-        justifyContent: "center",
-        alignItems: "center",
-        display: "flex",
-        flexDirection: "column",
+        justifyContent: 'center',
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
       }}
     >
       <p className="text">
@@ -16,30 +17,30 @@ const About = () => {
         provides a web authentication interface for users. It leverages the
         @passwordless-id/webauthn library to handle the registration and login
         processes securely.
-        {"\n"} {"\n"}
+        {'\n'} {'\n'}
         When users access the WebAuthPage, they are presented with a form that
         displays their registration status. If they are already registered, a
         message informs them that they have an account. Otherwise, they are
         prompted to enter a username to register.
-        {"\n"} {"\n"}
-        The registration process begins when users click the "Register" button.
-        This triggers the register function, which communicates with the web
-        authentication client. The client generates a challenge and initiates
-        the registration process. Upon successful registration, the user's
-        credential ID is stored in the local storage, associating it with the
-        provided username.
-        {"\n"} {"\n"}
-        To log in, users enter their username and click the "Login" button. The
-        login function is called, initiating the authentication process. The web
-        authentication client verifies the user's identity using their stored
-        credential ID and the challenge generated during login. If
-        authentication succeeds, the user is considered authenticated, and the
-        authentication data is displayed on the page.
-        {"\n"} {"\n"}
+        {'\n'} {'\n'}
+        The registration process begins when users click the
+        &quot;Register&quot; button. This triggers the register function, which
+        communicates with the web authentication client. The client generates a
+        challenge and initiates the registration process. Upon successful
+        registration, the user&apos;s credential ID is stored in the local
+        storage, associating it with the provided username.
+        {'\n'} {'\n'}
+        To log in, users enter their username and click the &quot;Login&quot;
+        button. The login function is called, initiating the authentication
+        process. The web authentication client verifies the user&apos;s identity
+        using their stored credential ID and the challenge generated during
+        login. If authentication succeeds, the user is considered authenticated,
+        and the authentication data is displayed on the page.
+        {'\n'} {'\n'}
         If the web authentication client is not available, a message is
         displayed indicating that web authentication is not supported in the
-        user's browser.
-        {"\n"} {"\n"}
+        user&apos;s browser.
+        {'\n'} {'\n'}
         The WebAuthPage component provides a simple and secure way for users to
         register and authenticate using web authentication. It demonstrates the
         usage of the @passwordless-id/webauthn library and showcases the power
